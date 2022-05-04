bdafa494c79e83092f1fcaf8236359aec017d0ca
Anton Bozhko <anton.bozhko04@gmail.com>
2022-05-04 15:19:31 +0300

    calc change

--- python/CalcV1.py
+++ python/CalcV1.py
@@ -1,5 +1,5 @@
 a = int(input("Первое чило: "))  # ты тут вводишь ага да
 b = int(input("Второе число: "))  # опа, и тут тоже водишь
 
-print("Сумма =", a+b, "eblo ti tupoe")
+print("Сумма =", a+b)
 input('Press ENTER to exit')
